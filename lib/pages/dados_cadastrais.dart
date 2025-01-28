80f05aed6cdaeffc0b82a3f907dbedd5ede7cf8b
reynatojunior <reynato.junior@hotmail.com>
2025-01-28 16:49:45 -0300

    commit experience

--- lib/pages/dados_cadastrais.dart
+++ lib/pages/dados_cadastrais.dart
@@ -1,14 +1,86 @@
 import 'package:flutter/material.dart';
+import 'package:login/repositories/nivel_repository.dart';
+import 'package:login/shared/widgets/text_label.dart';
 
-class DadosCadastraisPage extends StatelessWidget {
+class DadosCadastraisPage extends StatefulWidget {
   const DadosCadastraisPage({super.key});
 
   @override
+  State<DadosCadastraisPage> createState() => _DadosCadastraisPageState();
+}
+
+class _DadosCadastraisPageState extends State<DadosCadastraisPage> {
+  TextEditingController nomeController = TextEditingController(text: "");
+  TextEditingController dataNascimentoController =
+      TextEditingController(text: "");
+  DateTime? dataNascimento;
+  var nivelRepository = NivelRepository();
+  var niveis = [];
+  var nivelSelecionado = "";
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    niveis = nivelRepository.retornaNiveis();
+
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: Text("Dados Cadastrais"),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.lightBlueAccent,
+          title: Text("Meus dados"),
+        ),
+        body: Padding(
+          padding: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            TextLabel(texto: "Nome"),
+            TextField(
+              controller: nomeController,
+            ),
+            TextLabel(texto: "Data de Nascimento"),
+            TextField(
+              controller: dataNascimentoController,
+              readOnly: true,
+              onTap: () async {
+                var data = await showDatePicker(
+                    context: context,
+                    firstDate: DateTime(1900, 1, 1),
+                    lastDate: DateTime(2030, 12, 31));
+                if (data != null) {
+                  dataNascimentoController.text = data.toString();
+                  dataNascimento = data;
+                }
+              },
+            ),
+            TextLabel(texto: "Nível de experiência"),
+            Column(
+                children: niveis
+                    .map((nivel) => RadioListTile(
+                        dense: true,
+                        title: Text(nivel.toString()),
+                        selected: nivelSelecionado == nivel,
+                        value: nivel,
+                        groupValue: nivelSelecionado,
+                        onChanged: (value) {
+                          print(value);
+                          setState(() {
+                            nivelSelecionado = value.toString();
+                          });
+                        }))
+                    .toList()),
+            TextButton(
+              onPressed: () {
+                print(nomeController.text);
+              },
+              child: Text("Salvar"),
+            ),
+          ]),
+        ),
       ),
     );
   }
---
+++ lib/shared/widgets/text_label.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TextLabel extends StatelessWidget {
+  final String texto;
+  const TextLabel({super.key, required this.texto});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 10),
+      child: Text(texto,
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.w700)),
+    );
+  }
+}
